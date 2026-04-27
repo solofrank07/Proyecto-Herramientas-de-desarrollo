da54989b6242c10ff8709f8b10ea1f6751c8759f
solofrank07 <fgutierresbpacherres@gmail.com>
2026-04-27 00:35:08 -0500

    Subiendo mi proyecto completo

---
+++ src/main/java/com/example/HerramientaTrabajo/HerramientaTrabajoApplication.java
@@ -0,0 +1,13 @@
+package com.example.HerramientaTrabajo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerramientaTrabajoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerramientaTrabajoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HerramientaTrabajo/HerramientaTrabajoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HerramientaTrabajo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerramientaTrabajoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
